e2d51904a770284d4708fa94faf47150577645cb
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-09-11 13:29:06 +0500

    Task2

---
+++ Tyuiu.BocharovaES.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BocharovaES.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BocharovaES.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Екатерина";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Екатерина", res);
+        }
+    }
+}
